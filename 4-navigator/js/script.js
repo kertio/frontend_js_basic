22e6450da4da83a96bd5211d19f853058fe03484
kertio <_root_@list.ru>
2023-09-19 12:01:55 +0500

    fix: Исправление ошибок.

--- 4-navigator/js/script.js
+++ 4-navigator/js/script.js
@@ -4,11 +4,14 @@ const addressLong = 8;
 const positionLat = 3;
 const positionLong = 2;
 
-const pathLat = addressLat - positionLat;
-const pathLong = addressLong - positionLong;
+const pathLat = (addressLat - positionLat) ** 2;
+const pathLong = (addressLong - positionLong) ** 2;
+
+const distance =  Math.sqrt(pathLat + pathLong).toFixed(2);
 
 console.log(`Текущее положение:
-${positionLat}:${positionLong}
-Пункт назначения находится на расстоянии:
-${pathLat}:${pathLong}`);
+X=${positionLat} Y=${positionLong}
+Пункт назначения находится в точке:
+X=${addressLat} Y=${addressLong}
+Расстояние между точками: ${distance}`);
 
--- 5-languages/js/script.js
+++ 5-languages/js/script.js
@@ -11,3 +11,17 @@ switch (lang) {
 		console.log('Guten tag');
 		break;
 }
+
+lang = 'en';
+
+switch (lang) {
+	case 'ru':
+		console.log('Добрый день');
+		break;
+	case 'en':
+		console.log('Good afternoon');
+		break;
+	case 'de':
+		console.log('Guten tag');
+		break;
+}
